a2e068d0f655fe434e579a853b633dead2de6129
allseenn <allseen@live.ru>
2024-07-12 05:06:21 +0300

    data

--- datasets.py
+++ datasets.py
@@ -6,12 +6,17 @@ import json
 url = "https://apidata.mos.ru/v1"
 
 folder = "/datasets/"
+dataset = "2072"
 params = {
     "api_key": DATA_MOS_RU,
 }
 
-response = requests.get(url+folder, params=params)
+# filename = "datasets.json"
+filename = "data-2072.json" # ЕГЭ
+# filename = "data-2263.json" # Список школ
+# filename = "data-2457.json" # Экология
+response = requests.get(url+folder+dataset, params=params)
 data = json.loads(response.text)
-with open("datasets.json", "w", encoding="utf-8", errors="ignore") as outfile:
+with open(filename, "w", encoding="utf-8", errors="ignore") as outfile:
     json.dump(data, outfile, ensure_ascii=False, indent=2)
 
